93cbf0b24f3d5b82a7cc456a5243adda48bbff63
galdinorenantruta <galdinotrutarenan@gmail.com>
2024-01-28 19:53:27 -0300

    navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,22 @@ import { Container } from "react-bootstrap"
 import Home from "./pages/Home"
 import Store from "./pages/Store"
 import About from "./pages/About"
+import Navbar from "./components/Navbar"
 
 function App() {
   
 
   return (
+    <>
+    <Navbar/>
     <Container>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/store" element={<Store/>}/>
       <Route path="/about" element={<About/>}/>
     </Routes>
-    </Container>
+    </Container></>
+    
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { ImCart } from "react-icons/im";
+import {
+  Container,
+  Nav,
+  Navbar as BootstrapNavbar,
+  Button,
+} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <BootstrapNavbar sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <NavLink to={"/"}>Home</NavLink>
+          <NavLink to={"/about"}>Home</NavLink>
+          <NavLink to={"/store"}>Home</NavLink>
+        </Nav>
+        <Button
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+          variant="outline-primary"
+          className="rounded-circle"
+        >
+          <ImCart />
+          <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+              style={{
+                color: "white",
+                width: "1.5rem",
+                height: "1.5rem",
+                position: "absolute",
+                bottom: 0,
+                right: 0,
+                transform: "translate(25%, 25%)",
+              }}
+            >
+              
+          3 
+            </div>
+        </Button>
+       
+      </Container>
+    </BootstrapNavbar>
+  );
+};
+
+export default Navbar;
